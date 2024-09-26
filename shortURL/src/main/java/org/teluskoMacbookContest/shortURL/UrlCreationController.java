097b777b4460ed5370e043ad64ea79ed26e508e2
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2024-09-26 15:35:14 +0530

    Implementation done!

---
+++ shortURL/src/main/java/org/teluskoMacbookContest/shortURL/UrlCreationController.java
@@ -0,0 +1,46 @@
+package org.teluskoMacbookContest.shortURL;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class UrlCreationController {
+    private final UrlService service;
+    @Autowired
+    public UrlCreationController(UrlService service){
+        this.service = service;
+    }
+    @GetMapping("/")
+    public String index(){
+        return "Index Page";
+    }
+    @GetMapping("/urls")
+    public List<UrlPair> getOriginalUrls(){
+        return service.getAll();
+    }
+    @GetMapping("/shorter")
+    public ResponseEntity<UrlPair> handleUrl(@RequestParam("url") String oUrl){
+        System.out.println(oUrl);
+        UrlPair url = service.handleOUrl(oUrl);
+        return ResponseEntity.status(HttpStatus.CREATED).body(url);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public void getOriginalUrl(@PathVariable("shortUrl") String sUrl, HttpServletResponse httpResponse) throws IOException {
+        // should redirect the page to original url
+        try{
+            String output = service.getOUrl(sUrl);
+            httpResponse.sendRedirect(output);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
